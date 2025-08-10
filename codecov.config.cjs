f8762325371c4a68eb3df9428a7e6506036fc01b
phothinmg <phothinmg@disroot.org>
2025-08-11 01:10:34 +0630

    deleted:    coverage/codecov.json

--- codecov.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "Textile Ts",
-  outputDir: "./coverage",
-  reports: [["codecov"]],
-  entryFilter: {
-    "**/node_modules/**": false,
-    "**/src/**": true,
-  },
-};
